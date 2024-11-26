86746f1fae7561ef04dbc581684e04a3be95e26c
AhmedMohamedShalaby <shalabya279@gmail.com>
2024-11-26 20:50:44 +0200

    init project and configer

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+    <h1 className="text-3xl text-white text-center  font-bold underline">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
