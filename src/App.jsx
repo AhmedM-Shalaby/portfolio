deb29cedf99cd9785afd6365d33f25246382beef
AhmedMohamedShalaby <shalabya279@gmail.com>
2025-01-07 00:02:24 +0200

    winp

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import Layout from "./Components/Layout";
+
 function App() {
 
   return (
     <>
-    <h1 className="text-3xl text-white text-center  font-bold underline">
-      Hello world!
-    </h1>
+    <Layout/>
     </>
   )
 }
