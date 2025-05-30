459d79d7645cd7d97ac83c9c835c2b5f006a489a
AhmedM-Shalaby <shalabya279@gmail.com>
2025-05-31 02:21:22 +0300

    Fix SPA routing for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,15 @@ import About from "./layout/About";
 function App() {
   const myRouter = createBrowserRouter([
     {
-      path: "",
+      path: "/",
       element: <Layout />,
       children: [
-        { path: "/about", element: <About /> },
-        { path: "/intouch", element: <InTouch /> },
-        { path: "/portfolio", element: <MyProjects /> },
+        { path: "about", element: <About /> },
+        { path: "intouch", element: <InTouch /> },
+        { path: "portfolio", element: <MyProjects /> },
       ],
     },
-    { path: "/portfolio/:projectId", element: <ProjectDetails /> },
+    { path: "portfolio/:projectId", element: <ProjectDetails /> },
   ]);
   return <RouterProvider router={myRouter}></RouterProvider>;
 }
