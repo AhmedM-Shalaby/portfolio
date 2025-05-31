6f65cb6dca08070d0a6ec6f5607de9135987ef89
AhmedM-Shalaby <shalabya279@gmail.com>
2025-05-31 22:10:56 +0300

    up data Cv

--- src/Components/personInfo.jsx
+++ src/Components/personInfo.jsx
@@ -1,7 +1,7 @@
 import { FaUser, FaCloudDownloadAlt } from "react-icons/fa";
 function PersonInfo() {
   const linkCV =
-    "https://drive.google.com/file/d/1wmZMNqYYzEWufQbID8ezkE5_w9wSM_vF/view?usp=sharing";
+    "https://drive.google.com/file/d/1JVeDoYnCGdqoUWbDnuSw8yPau_opInhm/view?usp=sharing";
   const handleClick = async () => {
     window.open(linkCV, "_blank");
     const response = await fetch(linkCV);
